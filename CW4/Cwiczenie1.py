58e336f8f57d9a90934fd5a8870bbe31a9697ecf
Igor Barcikowski <150329028+Barcik04@users.noreply.github.com>
2024-03-29 17:10:31 +0100

    Add files via upload

---
+++ CW4/Cwiczenie1.py
@@ -0,0 +1,12 @@
+names = ["michal", "nela", "ola", "przemek"]
+
+duze = [x[0].upper() + x[1:].lower() for x in names]
+print(duze)
+
+tylko_i = [x for x in duze if 'i' in x]
+print(tylko_i)
+
+tupla = ("Ola", "Marta", "Gosia", "Magda", "Baba")
+dlugosc_all = sum(len(x) for x in tupla)
+
+print(dlugosc_all)
