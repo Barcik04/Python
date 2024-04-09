5c875d460788db2c27bfc6633325e7dd77352e35
Igor Barcikowski <150329028+Barcik04@users.noreply.github.com>
2024-04-09 16:05:54 +0200

    Create zad1.py

---
+++ Cw5/zad1.py
@@ -0,0 +1,17 @@
+class Car:
+
+    def __init__(self, marka, rok):
+        self.marka = marka
+        self.rok = rok
+
+car1 = Car("Maserati", "1998")
+car2 = Car("Porshe", "2018")
+
+print(car1.marka, car1.rok)
+print(car2.marka, car2.rok)
+
+car1 = car2
+
+print(car1.marka, car1.rok)
+print(car2.marka, car1.rok)
+print(car2 == car1)
