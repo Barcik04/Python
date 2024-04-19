7bdea9dd036c977b2b6fe4d972245667554f632f
Igor Barcikowski <150329028+Barcik04@users.noreply.github.com>
2024-04-19 11:23:38 +0200

    Update and rename awdhaw.py to Zad1_i_2.py

--- CW8/Zad1_i_2.py
+++ CW8/Zad1_i_2.py
@@ -1,4 +1,8 @@
+
 import numpy as np
+
+
+print(f'Czwiczenie 1\n')
 
 aria = np.array([i for i in range(10,40,2)])
 print(np.shape(aria)[0])
@@ -14,12 +18,12 @@ def change(A, x):
     return nowa
 print(f'{change(aria, x)}\n\n\n')
 
-
+print(f'\n\n\n\nCzwiczenie 2\n')
 
 A = np.array([[1,1,2],[2,1,0],[4,1,2]])
 B = np.array([[2,5,7],[2,8,0],[4,3,1]])
 
-print(A+B) 
+print(A+B)
 print(f'\n')
 print(np.dot(A,B)) #mnozenie macierzy
 print(f'\n')
@@ -32,3 +36,30 @@ print(f'\n')
 print(A**5) #kazdy element do ^5
 print(f'\n')
 print(np.linalg.matrix_power(A,5)) #macierz do ^5
+print(f'\n')
+print(np.linalg.det(B)) #wyznacznik
+print(f'\n')
+print(np.linalg.matrix_power(B, -3)) #do potegi -3
+print(f'\n\n\n')
+
+
+C = np.array([[1],[2],[4]])
+D = np.array([[2,5,7]])
+
+print(C * D)
+print(f'\n')
+print(D * C)
+print(f'\n')
+print(np.multiply(C,D))
+print(f'\n\n\n')
+
+
+E = np.array([[1,5],[2,1]])
+F = np.array([[2,1],[2,8]])
+
+print(E/F)
+print(f'\n')
+print(E//F)
+print(f'\n')
+print(E%F)
+
