c2c521144740034ed2a8fb3d02bbac5ebd092d02
Igor Barcikowski <150329028+Barcik04@users.noreply.github.com>
2024-05-24 10:24:45 +0200

    Update kolos_2.py

--- kolos_2.py
+++ kolos_2.py
@@ -5,7 +5,16 @@ NUMPY
         arr = np.array([1, 2, 3, 4, 5])
         result = np.where(arr > 2, arr * 2, arr - 2)
         print(result)
-        # Output: [-1  0  6  8 10] explain
+        # Output: [-1  0  6  8 10]
         
         The np.where function works as follows: np.where(condition, x, y) returns elements chosen from x or y depending on the condition. 
         If the condition is True for an element, the corresponding element from x is taken; otherwise, the element from y is taken.
+
+    SUM
+        arr = np.array([[1, 2, 3], [4, 5, 6]])
+        result = np.sum(arr, axis=0)
+        print(result)
+        # Output: [5 7 9]
+        
+        axis=0: This means we want to sum the elements along the vertical axis (i.e., column-wise summation).
+
